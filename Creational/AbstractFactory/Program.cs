44c47c78d644e127b6e82e7ad49e7acc38a98c39
Mikhail <mishachesnokov111@gmail.com>
2018-02-09 22:20:11 +0500

    Refactor

---
+++ Creational/AbstractFactory/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Creational.AbstractFactory
+{
+    interface IAbstractFactory
+    {
+        IAbsractProbuctA CreateProductA();
+        IAbstractProductB CreateProductB();
+    }
+
+    interface IAbsractProbuctA
+    {
+        void Interact(IAbstractProductB productB);
+    }
+
+    interface IAbstractProductB
+    {
+        void Interact(IAbsractProbuctA probuctA);
+    }
+
+    class ConcreteProbuctA1 : IAbsractProbuctA
+    {
+        public void Interact(IAbstractProductB productB)
+        {
+            Console.WriteLine("The instance of " +
+                              this.GetType().Name + " is interacting with the instance of " + productB.GetType().Name);
+        }
+    }
+
+    class ConcreteProductA2 : IAbsractProbuctA
+    {
+        public void Interact(IAbstractProductB productB)
+        {
+            Console.WriteLine("The instance of " +
+                              this.GetType().Name + " is interacting with the instance of " + productB.GetType().Name);
+        }
+    }
+
+    class ConcreteProductB1 : IAbstractProductB
+    {
+        public void Interact(IAbsractProbuctA probuctA)
+        {
+            Console.WriteLine("The instance of " +
+                              this.GetType().Name + " is interacting with the instance of " + probuctA.GetType().Name);
+        }
+    }
+
+    class ConcreteProductB2 : IAbstractProductB
+    {
+        public void Interact(IAbsractProbuctA probuctA)
+        {
+            Console.WriteLine("The instance of " +
+                              this.GetType().Name + " is interacting with the instance of " + probuctA.GetType().Name);
+        }
+    }
+
+    class ConcreteFactory1 : IAbstractFactory
+    {
+        public IAbsractProbuctA CreateProductA()
+        {
+            return new ConcreteProbuctA1();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB1();
+        }
+    }
+
+    class ConcreteFactory2 : IAbstractFactory
+    {
+        public IAbsractProbuctA CreateProductA()
+        {
+            return new ConcreteProductA2();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB2();
+        }
+    }
+
+    class Client
+    {
+        private readonly IAbsractProbuctA _probuctA;
+        private readonly IAbstractProductB _productB;
+
+        public Client(IAbstractFactory factory)
+        {
+            _probuctA = factory.CreateProductA();
+            _productB = factory.CreateProductB();
+        }
+
+        public void DoInteract()
+        {
+            _probuctA.Interact(_productB);
+            _productB.Interact(_probuctA);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Client client1 = new Client(new ConcreteFactory1());
+
+            client1.DoInteract();
+
+
+            Client client2 = new Client(new ConcreteFactory2());
+
+            client2.DoInteract();
+        }
+    }
+}
---
+++ Structural/Decorator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Structural.Decorator
+{
+    interface IComponent
+    {
+        void Operation();
+    }
+
+    class ConcreteComponent : IComponent
+    {
+        public void Operation()
+        {
+            Console.Write("World!");
+        }
+    }
+
+    abstract class Decorator : IComponent
+    {
+        protected IComponent Component { get; }
+
+        protected Decorator(IComponent component)
+        {
+            Component = component;
+        }
+
+        public virtual void Operation()
+        {
+            Component.Operation();
+        }
+    }
+
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(IComponent component)
+            : base(component) { }
+
+        public override void Operation()
+        {
+            Console.Write(", ");
+
+            base.Operation();
+        }
+    }
+
+    class ConcreteDecoratorA : Decorator
+    {
+        public ConcreteDecoratorA(IComponent component)
+            : base(component) { }
+
+        public override void Operation()
+        {
+            Console.Write("Hello");
+
+            base.Operation();
+        }
+    }
+
+    static class Program
+    {
+        static void Main()
+        {
+            Decorator decorator = new ConcreteDecoratorA(new ConcreteDecoratorB(new ConcreteComponent()));
+
+            decorator.Operation(); // Hello, World!
+        }
+    }
+}
